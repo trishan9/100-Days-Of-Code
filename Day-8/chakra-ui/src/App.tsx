d40644b89e6cb4f5492968082fea6df31513ad10
Trishan Wagle <mailtotrishan@gmail.com>
2023-07-31 20:43:23 +0545

    day8: day8 completed

---
+++ Day-8/chakra-ui/src/App.tsx
@@ -0,0 +1,175 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  Stack,
+  Alert,
+  AlertIcon,
+  Card,
+  Heading,
+  Text,
+  CardBody,
+  Image,
+  CardFooter,
+  Button,
+  Divider,
+  ButtonGroup,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  useSteps,
+  Stepper,
+  Step,
+  StepIndicator,
+  StepStatus,
+  StepIcon,
+  StepNumber,
+  StepSeparator,
+  StepTitle,
+  StepDescription,
+} from "@chakra-ui/react";
+
+const steps = [
+  { title: "First", description: "Contact Info" },
+  { title: "Second", description: "Date & Time" },
+  { title: "Third", description: "Select Rooms" },
+];
+
+const App = () => {
+  const { activeStep } = useSteps({
+    index: 1,
+    count: steps.length,
+  });
+  return (
+    <>
+      <Accordion>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Section 1 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Section 2 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+
+      <Stack spacing={3}>
+        <Alert status="error">
+          <AlertIcon />
+          There was an error processing your request
+        </Alert>
+
+        <Alert status="success">
+          <AlertIcon />
+          Data uploaded to the server. Fire on!
+        </Alert>
+
+        <Alert status="warning">
+          <AlertIcon />
+          Seems your account is about expire, upgrade now
+        </Alert>
+
+        <Alert status="info">
+          <AlertIcon />
+          Chakra is going live on August 30th. Get ready!
+        </Alert>
+      </Stack>
+      <Card maxW="sm">
+        <CardBody>
+          <Image
+            src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Living room Sofa</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+            <Text color="blue.600" fontSize="2xl">
+              $450
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid" colorScheme="blue">
+              Buy now
+            </Button>
+            <Button variant="ghost" colorScheme="blue">
+              Add to cart
+            </Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+
+      <Breadcrumb spacing="8px" separator={"-"}>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#">About</BreadcrumbLink>
+        </BreadcrumbItem>
+
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink href="#">Contact</BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+
+      <Stepper index={activeStep}>
+        {steps.map((step, index) => (
+          <Step key={index}>
+            <StepIndicator>
+              <StepStatus
+                complete={<StepIcon />}
+                incomplete={<StepNumber />}
+                active={<StepNumber />}
+              />
+            </StepIndicator>
+
+            <Box flexShrink="0">
+              <StepTitle>{step.title}</StepTitle>
+              <StepDescription>{step.description}</StepDescription>
+            </Box>
+
+            <StepSeparator />
+          </Step>
+        ))}
+      </Stepper>
+    </>
+  );
+};
+
+export default App;
